104a6dd4857601b9369ac4ebd7676ffe19429bab
aaron24-web <Aarontunwork24@gmail.com>
2025-06-20 10:36:11 -0600

    interfaz grafica

--- Program.cs
+++ Program.cs
@@ -97,15 +97,14 @@ builder.Services.AddSwaggerGen(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+
     app.UseSwagger();
     app.UseSwaggerUI(options =>
     {
         options.SwaggerEndpoint("/swagger/v1/swagger.json", "Tu API V1");
         options.RoutePrefix = string.Empty;
     });
-}
+
 
 // app.UseHttpsRedirection();
 
--- obj/Debug/net8.0/Proyect_api.AssemblyInfo.cs
+++ obj/Debug/net8.0/Proyect_api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proyect_api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2edff75420e4327d6e856eb458123befd9deed4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6f2808c9e9caf885245015afc63210d673809eb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proyect_api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Proyect_api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
